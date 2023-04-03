52ffb69a14a306da34ca372e70d51ff4b6106e74
Miaofei <ameision@hotmail.com>
2023-04-03 10:22:28 -0700

    attempt deadlock fix
    Signed-off-by: Miaofei <ameision@hotmail.com>

--- rs_taskflow/src/execution.rs
+++ rs_taskflow/src/execution.rs
@@ -1,7 +1,11 @@
 use std::future::Future;
 use std::pin::Pin;
 use std::sync::{Arc, Condvar, Mutex, MutexGuard};
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering::Relaxed;
 use std::task::{Context, Poll};
+use std::thread;
+use std::time::Duration;
 
 use tokio::task;
 use tokio::task::JoinHandle;
@@ -10,26 +14,23 @@ use crate::dag::node::NodeId;
 use crate::flow::{Flow, TaskHandle};
 use crate::task::*;
 
-struct ExecTaskJoinHandle {
-    join_handle: JoinHandle<()>,
-    completed: bool,
-}
-
 struct ExecTask {
-    lock: Mutex<Option<ExecTaskJoinHandle>>,
+    join_handle: Mutex<Option<JoinHandle<()>>>,
+    completed: AtomicBool,
     condvar: Condvar,
 }
 
 impl ExecTask {
     fn new() -> Self {
         Self {
-            lock: Mutex::new(Option::None),
+            join_handle: Mutex::new(None),
+            completed: AtomicBool::new(false),
             condvar: Condvar::new(),
         }
     }
 
-    fn lock(&self) -> MutexGuard<Option<ExecTaskJoinHandle>> {
-        return self.lock.lock().unwrap();
+    fn lock(&self) -> MutexGuard<Option<JoinHandle<()>>> {
+        return self.join_handle.lock().unwrap();
     }
 
     fn notify(&self) {
@@ -48,20 +49,25 @@ impl Future for ExecTaskFuture {
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         for from_node_id in self.flow.get_flow_graph().get_dependencies(self.node_id) {
-            if cfg!(debug_assertions) && self.futures[*from_node_id].lock().is_none() {
-                println!("Node {} is missing a future / join handle!", *from_node_id)
+            if self.futures[*from_node_id].completed.load(Relaxed) {
+                continue;
             }
 
-            let mut locked_guard = self.futures[*from_node_id].lock();
-            let join_handle = locked_guard.as_mut().unwrap();
-            if join_handle.completed {
-                continue;
-            } else {
-                match Pin::new(&mut join_handle.join_handle).poll(cx) {
-                    Poll::Ready(_) => continue,
-                    Poll::Pending => return Poll::Pending,
+            if cfg!(debug_assertions) {
+                println!("{:?} Visiting node id {} checking dependent node id {}", thread::current().id(), self.node_id, *from_node_id);
+            }
+            match Pin::new(&mut self.futures[*from_node_id].lock().as_mut().unwrap()).poll(cx) {
+                Poll::Ready(_) => {
+                    continue;
+                }
+                Poll::Pending => {
+                    return Poll::Pending;
                 }
             }
+        }
+
+        if cfg!(debug_assertions) {
+            println!("{:?} Visiting node id {} ready", thread::current().id(), self.node_id);
         }
 
         self.flow
@@ -70,11 +76,7 @@ impl Future for ExecTaskFuture {
             .get_mut_value()
             .exec(self.flow.as_ref());
 
-        self.futures[self.node_id]
-            .lock()
-            .as_mut()
-            .unwrap()
-            .completed = true;
+        self.futures[self.node_id].completed.store(true, Relaxed);
         self.futures[self.node_id].notify();
 
         Poll::Ready(())
@@ -90,25 +92,22 @@ impl Execution {
         Execution { flow: flow }
     }
 
-    fn spawn_exec_task(&self, node_id: usize, futures: Arc<Vec<ExecTask>>) {
+    fn spawn_exec_task(&self, node_id: usize, futures: &Arc<Vec<ExecTask>>) {
         // if cfg!(debug_assertions) {
         //     println!("Adding future for node {}", node_id);
         // }
 
-        let task_future = task::spawn(ExecTaskFuture {
+        let join_handle = task::spawn(ExecTaskFuture {
             flow: self.flow.clone(),
             node_id,
             futures: futures.clone(),
         });
-        *futures[node_id].lock() = Option::Some(ExecTaskJoinHandle {
-            join_handle: task_future,
-            completed: false,
-        });
+        *futures[node_id].lock() = Some(join_handle);
 
         // if cfg!(debug_assertions) {
         //     match *futures.clone()[node_id].lock() {
-        //         Option::Some(_) => println!("Node {} has a future!", node_id),
-        //         Option::None => println!("Node {} has no future!", node_id),
+        //         Some(_) => println!("Node {} has a future!", node_id),
+        //         None => println!("Node {} has no future!", node_id),
         //     }
         // }
     }
@@ -116,31 +115,40 @@ impl Execution {
     pub async fn start_and_finish(self) -> Self {
         let len = self.flow.get_num_tasks();
 
-        let mut futures_vec = Vec::<ExecTask>::with_capacity(len);
+        let mut task_execs = Vec::<ExecTask>::with_capacity(len);
         for _ in 0..len {
-            futures_vec.push(ExecTask::new());
+            task_execs.push(ExecTask::new());
         }
 
         // each future will have a copy of this Arc
-        let futures_vec_arc = Arc::new(futures_vec);
+        let task_execs_arc = Arc::new(task_execs);
 
         let bfs = self.flow.get_flow_graph().build_bfs().unwrap();
         while let Some(node) = bfs.next() {
-            if cfg!(debug_assertions) {
-                println!("  Visiting node id {}", node.get_id());
-            }
-
             bfs.visited_node(&*node);
-
-            let node_id = node.get_id();
-            let futures_vec_copy = futures_vec_arc.clone();
-            self.spawn_exec_task(node_id, futures_vec_copy);
+            self.spawn_exec_task(node.get_id(), &task_execs_arc);
         }
 
-        for future_iter in futures_vec_arc.iter() {
-            let mut locked_guard = future_iter.lock();
-            while !locked_guard.as_ref().unwrap().completed {
-                locked_guard = future_iter.condvar.wait(locked_guard).unwrap();
+        for (id, task_exec) in task_execs_arc.iter().enumerate() {
+            while !task_exec.completed.load(Relaxed) {
+                if cfg!(debug_assertions) {
+                    println!("{:?} Waiting on node id {}", thread::current().id(), id);
+                }
+
+                // let result = task_exec
+                //     .condvar
+                //     .wait_timeout(task_exec.lock(), Duration::from_secs(1))
+                //     .unwrap();
+                // if result.1.timed_out() {
+                //     continue;
+                // } else {
+                //     break;
+                // }
+
+                thread::sleep(Duration::from_secs(1));
+            }
+            if cfg!(debug_assertions) {
+                println!("{:?} Node id {} is complete", thread::current().id(), id);
             }
         }
 
